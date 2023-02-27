1b93d87567eeae1783e1a32e6ca1ace303731537
yen001004 <yenn001004@gmail.com>
2023-02-27 19:43:41 +0900

    feat: POST data with getServerSideProps

--- pages/projects.js
+++ pages/projects.js
@@ -1,8 +1,11 @@
 import Layout from "@/components/layout";
 import Head from "next/head";
 import React from "react";
+import { TOKEN, DATABASE_ID } from "@/config";
+import axios from "axios";
 
-const Projects = () => {
+export default function Projects({ result }) {
+  console.log(result);
   return (
     <Layout>
       <Head>
@@ -13,6 +16,32 @@ const Projects = () => {
       <h1>프로젝트</h1>
     </Layout>
   );
-};
+}
 
-export default Projects;
+// 빌드 타임에 호출
+export async function getServerSideProps() {
+  const options = {
+    method: "POST",
+    url: `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    headers: {
+      Accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    data: { page_size: 100 },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
